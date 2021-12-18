851469c8a43132c0e89fc4ea5f3bb2e100658ab8
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 06:10:16 +0300

    Scripted auto-commit on change (2021-12-18 06:10:16) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,27 +1,7 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
+
 
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("lab3");
-        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> DistOfAirportDelays = sc.textFile("664600583_T_ONTIME_sample_cutted.csv");
-        JavaRDD<String> DistOfAirportNames  = sc.textFile("L_AIRPORT_ID.csv");
-
-
-        JavaPairRDD<Integer, String> DataOfAirportNames = DistOfAirportNames.filter(str ->!str.contains("Code"))
-                .mapToPair(value -> {
-                    String[] Table = value.split("\",");
-                    int DestAirportID = Integer.parseInt((Table[0]).replaceAll("\"", ""));
-                    return new Tuple2<>(DestAirportID, Table[1]);
-                });
-        JavaPairRDD<Integer, Integer>, 
-
-    }
 }
--- src/main/java/SparkJob.java
+++ src/main/java/SparkJob.java
@@ -1,2 +1,25 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
 public class SparkJob {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> DistOfAirportDelays = sc.textFile("664600583_T_ONTIME_sample_cutted.csv");
+        JavaRDD<String> DistOfAirportNames  = sc.textFile("L_AIRPORT_ID.csv");
+
+
+        JavaPairRDD<Integer, String> DataOfAirportNames = DistOfAirportNames.filter(str ->!str.contains("Code"))
+                .mapToPair(value -> {
+                    String[] Table = value.split("\",");
+                    int DestAirportID = Integer.parseInt((Table[0]).replaceAll("\"", ""));
+                    return new Tuple2<>(DestAirportID, Table[1]);
+                });
+        JavaPairRDD<Integer, Integer>,
+
+    }
 }
