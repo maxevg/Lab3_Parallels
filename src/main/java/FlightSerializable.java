62e1628a03492fa3ee4b4d9b5a6ad5e453a8ec8a
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 05:58:02 +0300

    Scripted auto-commit on change (2021-12-18 05:58:02) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.Serializable;
@@ -7,5 +8,13 @@ public class FlightSerializable implements Serializable {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> DistOfAirportDelays = sc.textFile("664600583_T_ONTIME_sample_cutted.csv");
+        JavaRDD<String> DistOfAirportNames  = sc.textFile("664600583_T_ONTIME_sample_cutted.csv");
+
+
+        JavaRDD<ParsedData> spli)tted = di)stFi)le.map(
+                s -> new ParsedData(s, ai)rportsBroadcasted.value())
+
     }
 }
