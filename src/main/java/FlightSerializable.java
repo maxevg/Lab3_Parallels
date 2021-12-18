b7939b7cae2d679ac6e535fec8a08da8c2e38801
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 05:56:07 +0300

    Scripted auto-commit on change (2021-12-18 05:56:07) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,7 +1,11 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
     public static void main(String[] args) {
-
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
