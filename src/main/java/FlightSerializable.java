c49b3790db0209fcbb5a2d3fac94563a9d180581
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 06:13:47 +0300

    Scripted auto-commit on change (2021-12-18 06:13:47) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,7 +1,13 @@
 
-
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
+    private int DEST_AIRPORT_ID;
+    private float ARR_DELAY;
+    private float AIR_TIME;
+    private float CANCELLED;
+
+    public FlightSerializable() {}
+    
 
 }
