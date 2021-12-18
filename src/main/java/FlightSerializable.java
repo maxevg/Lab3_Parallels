cab525f089ed90c44b47ef45ff2f224211e761aa
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 06:14:52 +0300

    Scripted auto-commit on change (2021-12-18 06:14:52) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -9,11 +9,12 @@ public class FlightSerializable implements Serializable {
 
     public FlightSerializable() {}
 
-    public FlightWritable(int DEST_AIRPORT_ID, float ARR_DELAY, float AIR_TIME, float CANCELLED) {
+    public FlightSerializable(int DEST_AIRPORT_ID, float ARR_DELAY, float AIR_TIME, float CANCELLED) {
         this.DEST_AIRPORT_ID = DEST_AIRPORT_ID;
         this.ARR_DELAY = ARR_DELAY;
         this.AIR_TIME = AIR_TIME;
         this.CANCELLED = CANCELLED;
     }
+    
 
 }
